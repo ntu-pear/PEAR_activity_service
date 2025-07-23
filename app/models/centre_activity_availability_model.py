40d11f366a9a8525fe40ee65c18cf7a98f8d2207
bh555 <bohui1523@gmail.com>
2025-07-24 00:06:08 +0800

    Added Centre_activity_availability model and sql script to create centre_activity_availability table.

---
+++ app/models/centre_activity_availability_model.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Column, Integer, String, DateTime, Boolean, ForeignKey
+from sqlalchemy.orm import relationship
+from datetime import datetime
+from app.database import Base
+
+class CentreActivityAvailability(Base):
+    __tablename__ = "CENTRE_ACTIVITY_AVAILABILITY",
+    
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    centre_activity_id = Column(Integer, ForeignKey("CENTRE_ACTIVITY.id"), nullable=False)
+    is_deleted = Column(Boolean, nullable=False, default=False),
+    start_date = Column(DateTime, nullable=False, default=None),
+    end_date = Column(DateTime, nullable=False, default=None),
+    created_date = Column(DateTime, nullable=False, default=datetime.now())
+    modified_date = Column(DateTime, nullable=False, default=datetime.now(), onupdate=datetime.utcnow)
+    created_by_id = Column(String, nullable=False)
+    modified_by_id = Column(String, nullable=False)
+
+    centre_activity = relationship("Centre_Activity")
---
+++ app/sql/centre_activity_availability/create_centre_activity_availability_table.sql
@@ -0,0 +1,15 @@
+USE activity_service_dev;
+GO
+
+CREATE TABLE [dbo].[CENTRE_ACTIVITY_AVAILABILITY](
+    id                 INT           IDENTITY(1,1) NOT NULL CONSTRAINT pk_centre_activity_availability PRIMARY KEY,
+	centre_activity_id INT			 NOT NULL CONSTRAINT fk_centre_activity_availability_centre_activity_id FOREIGN KEY REFERENCES [dbo].[CENTRE_ACTIVITY](id),
+	is_deleted		   BIT			 NOT NULL CONSTRAINT df_centre_activity_availability_is_deleted DEFAULT (0),
+	start_time         DATETIME2(3)  NOT NULL,
+    end_time           DATETIME2(3)  NULL,
+	created_date       DATETIME2(3)  NOT NULL CONSTRAINT df_centre_activity_availability_created_date DEFAULT SYSUTCDATETIME(),
+    modified_date      DATETIME2(3)  NOT NULL CONSTRAINT df_centre_activity_availability_modified_date DEFAULT SYSUTCDATETIME(),
+    created_by_id      NVARCHAR(50)  NULL,
+    modified_by_id     NVARCHAR(50)  NULL
+);
+GO
