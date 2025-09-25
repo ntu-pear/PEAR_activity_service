5b58806af06df1668da19f2bb1d984a8638bf5df
Albert481 <albertyu481@gmail.com>
2025-09-25 23:19:36 +0800

    fixed test case

--- tests/unit/test_centre_activity_exclusion.py
+++ tests/unit/test_centre_activity_exclusion.py
@@ -121,7 +121,7 @@ def test_create_exclusion_success(
     result = create_centre_activity_exclusion(get_db_session_mock, payload, mock_supervisor_user)
 
     assert result is fake_obj
-    get_db_session_mock.add.assert_called_once_with(fake_obj)
+    assert get_db_session_mock.add.call_count == 2 # Once for exclusion, once for Outbox record
     get_db_session_mock.commit.assert_called_once()
     get_db_session_mock.refresh.assert_called_once_with(fake_obj)
 
